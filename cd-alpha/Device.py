ccee8dee7df5d810bdfd86a43ec41f90557d1c21
Will Brown <will.brown@chip-diagnostics.com>
2021-12-21 15:25:51 -0500

    Adding defaults for list_of_pumps

--- cd-alpha/Device.py
+++ cd-alpha/Device.py
@@ -16,9 +16,6 @@ class Device:
         - Name of the default script to be used on launch, if anohter script is not chosen with the 
         file chooser this is the protocol that will be run
 
-    PATH_TO_PROTOCOLS: str
-        - Absolute path location to the protocols folder on this device. By default
-        this should be a sub-folder in the v0/cd-alpha directory
     
     OPTIONAL
 
@@ -28,6 +25,12 @@ class Device:
     PUMP_SERIAL_ADDR: str
         - Serial address for the pump/pump network. Defaults to "/dev/ttyUSB0" on linux 
         which shouldn't need to be changed
+
+    LIST_OF_PUMPS: list[str]
+        - for r0 default is ["WASTE"]
+        - for v0 default is ["WASTE", "LYSATE"]
+        - Should only be changed if more pumps have been added to the network, length must 
+        match the len(PUMP_ADDR) AND len(PUMP_DIAMETER)
 
     PUMP_ADDR: int or list[int]
         - Default for r0: PUMP_ADDR = 0
@@ -39,6 +42,10 @@ class Device:
         - Default for v0: PUMP_DIAMETER = [12.4, 12.4]
         - For v0 the diameters will be applied in the same order of the pumps.
         Default is [WASTE, LYSATE]
+
+    PATH_TO_PROTOCOLS: str
+        - Absolute path location to the protocols folder on this device. By default
+        this should be a sub-folder in the v0/cd-alpha directory
     
     DEBUG_MODE: bool
         - Puts the program in debug mode, for dev use only (default is False)
@@ -49,7 +56,7 @@ class Device:
 
         try:
             with open(config_file_json) as f:
-                required_values = ["DEVICE_TYPE", "DEFAULT_PROTOCOL", "PATH_TO_PROTOCOL"]
+                required_values = ["DEVICE_TYPE", "DEFAULT_PROTOCOL"]
                 config_file_dict = json.loads(f.read())
                 for req in required_values:
                     if req not in config_file_dict.keys():
@@ -64,14 +71,21 @@ class Device:
 
             if not hasattr(self, "DEBUG_MODE"):
                 self.DEBUG_MODE = False
+
+            if not hasattr(self, "PATH_TO_PROTOCOLS"):
+                self.PATH_TO_PROTOCOLS = "/home/pi/cd-alpha/protocols/"
             
             # Set defaults based on device type
             if self.DEVICE_TYPE == "R0":
+                if not hasattr(self, "LIST_OF_PUMPS"):
+                    self.LIST_OF_PUMPS = ["WASTE"]
                 if not hasattr(self, "PUMP_ADDR"):
                     self.PUMP_SERIAL_ADDR = 0
                 if not hasattr(self, "PUMP_DIAMETER"):
                     self.PUMP_DIAMETER = 12.4
             elif self.DEVICE_TYPE == "V0":
+                if not hasattr(self, "LIST_OF_PUMPS"):
+                    self.LIST_OF_PUMPS = ["WASTE", "LYSATE"]
                 if not hasattr(self, "PUMP_ADDR"):
                     self.PUMP_SERIAL_ADDR = [1,2]
                 if not hasattr(self, "PUMP_DIAMETER"):
