33c8e5113ce1e6edfdbb9a8a50a22e9139fe9c76
Will Brown <will.brown@chip-diagnostics.com>
2022-01-18 11:47:12 -0500

    Trying to get unit tests working

--- cd_alpha/tests/test_protocol_chooser.py
+++ cd_alpha/tests/test_protocol_chooser.py
@@ -1,5 +1,5 @@
 import unittest
-from cd_alpha.ChipFlowApp import ProcessWindow
+from cd_alpha import ProcessWindow
 
 class ProtocolChooserTestCase(unittest.TestCase):
 
@@ -7,9 +7,15 @@ class ProtocolChooserTestCase(unittest.TestCase):
         # import class and prepare everything here.
         self.test_window = ProcessWindow()
 
+    # Test a standard protocol load, make sure all steps are present and in the right order
     def test_protocol_load(self):
         # create a test window and check that all of the required screens are added
         self.test_window.load_protocol("./v0-protocol-16v1.json")
+        self.assertEqual(len(self.test_window.process_sm.screens), 16)
+
+    # Test that loading protocols multiple times in a row doesn't cause duplicate steps
+
+    # Test that loading an invalid file raises a non fatal error
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='cd-alpha',
+setup(
+      name='cd_alpha',
       version='1.0',
       description='ChipDx App for V0 devices',
       author='Will Brown',
       author_email='will.brown@chip-diagnostics.com',
       url='https://www.chip-diagnostics.com/',
-      packages=[],
-     )
+      packages=["cd_alpha",]
+)
