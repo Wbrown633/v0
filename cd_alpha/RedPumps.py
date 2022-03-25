6a0e67b2b9688c2c8901e616fb2e390272d997b4
Will Brown <will.brown@chip-diagnostics.com>
2022-03-25 11:31:36 -0400

    initial work on RedPumps GUI

---
+++ cd_alpha/RedPumps.py
@@ -0,0 +1,15 @@
+import kivy
+kivy.require('2.1.0')
+#from NanoController import Nano
+#from NewEraPumps import PumpNetwork
+
+from kivy import App
+from kivy.uix.label import Label
+
+
+class RedPumps:
+    pass
+
+
+if __name__ == '__main__':
+    pass
