d87c5a1a47231b21be5ec3101859eee7444bffc3
Will Brown <will.brown@chip-diagnostics.com>
2022-04-08 13:57:06 -0400

    Try reading all lines from arduino when we get a response

--- cd_alpha/pressure_controller.py
+++ cd_alpha/pressure_controller.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
                     time.sleep(0.1) #wait for arduino to answer
                     while arduino.inWaiting()==0: pass
                     if  arduino.inWaiting()>0: 
-                        answer=arduino.readline()
+                        answer=arduino.readlines()
                         print(answer)
                         arduino.flushInput() #remove data after reading
             except KeyboardInterrupt:
