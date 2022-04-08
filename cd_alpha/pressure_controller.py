56ed327cbd6897f498811d7729d7266e86020e4e
Will Brown <will.brown@chip-diagnostics.com>
2022-04-08 13:52:50 -0400

    Add wait to listen for arduino response

--- cd_alpha/pressure_controller.py
+++ cd_alpha/pressure_controller.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                 while True:
                     cmd=input("Enter command : ")
                     arduino.write(cmd.encode())
-                    #time.sleep(0.1) #wait for arduino to answer
+                    time.sleep(0.1) #wait for arduino to answer
                     while arduino.inWaiting()==0: pass
                     if  arduino.inWaiting()>0: 
                         answer=arduino.readline()
