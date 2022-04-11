4a50f02ba5bbf5e6051076f2c3008403a28ec93e
Will Brown <will.brown@chip-diagnostics.com>
2022-04-11 11:46:19 -0400

    add back in wait for command response to come in

--- cd_alpha/PressureController.py
+++ cd_alpha/PressureController.py
@@ -31,7 +31,8 @@ class PressureController:
         time.sleep(0.5) #wait for arduino to answer
         if self.arduino.out_waiting > 0:
             self.arduino.reset_output_buffer()
-        time.sleep(0.5)
+        while self.arduino.out == 0:
+            pass
         if  self.arduino.in_waiting > 0: 
             confirmation_msg = self.arduino.readlines(self.arduino.in_waiting)
             self.arduino.reset_input_buffer
