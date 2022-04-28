7f21243812698fbd46fc35f9050d81bdb194e517
Will Brown <will.brown@chip-diagnostics.com>
2022-04-28 14:37:51 -0400

    create interactive shell for Pressure controller testing

--- cd_alpha/PressureController.py
+++ cd_alpha/PressureController.py
@@ -13,27 +13,29 @@ class PressureController:
             
     def __enter__(self) -> None: 
         logging.info("Opening Serial connection")
-        self.arduino = serial.Serial("/dev/ttyACM0", 115200, timeout=0)
-        time.sleep(0.1)
-        logging.info("{} connected!".format(self.arduino.port))
-        init_msg = self._send_command_str("")
-        logging.info("Got init msg: {}".format(init_msg))
+        self.arduino = serial.Serial("COM3", 115200, timeout=0) #for pi its "/dev/ttyACM0"
+        time.sleep(1)
+        print("{} connected!".format(self.arduino.port))
+        init_msg = self._read_input()
+        print("Got init msg: {}".format(init_msg))
         return self
         
     def __exit__(self, exc_type, exc_value, exc_traceback): 
-        logging.info("Closing serial port.")
+        print("Closing serial port.")
+        self.arduino.flush()
         self.arduino.close()
 
     def _send_command_str(self, cmd: str) -> List[str]:
         assert self.arduino.isOpen(), "Serial port not open."
         self.arduino.write(cmd.encode())
-        time.sleep(1) #wait for arduino to answer
-        while self.arduino.in_waiting == 0:
-            pass
-        while  self.arduino.in_waiting > 0:
-            time.sleep(0.1)
-            confirmation_msg = self.arduino.readlines(self.arduino.in_waiting)
-            self.arduino.reset_input_buffer()
+        list_of_input = self._read_input()
+        return list_of_input
+
+    def _read_input(self) -> str:
+        confirmation_msg = []
+        time.sleep(1.5)
+        while self.arduino.in_waiting > 0:
+            confirmation_msg.append(self.arduino.readline().decode())
         return confirmation_msg
 
     def _switch_status(self, switch_name:str, status:bool) -> str:
@@ -41,7 +43,7 @@ class PressureController:
         switch_status = 0
         if status:
             switch_status = 1
-        cmd_string = "{}:{};".format(switch_name, switch_status)
+        cmd_string = "{}:{};\n".format(switch_name, switch_status)
         response = self._send_command_str(cmd_string)
         return response
         
@@ -55,9 +57,23 @@ class PressureController:
     def set_pressure_pump(self, pressure_set_kPa: float) -> str:
         '''Change the pressure set point for the pump.'''
         assert pressure_set_kPa < self.max_pressure_set_point_kPa, "Invalid pressure"
-        cmd_string = "PUMP:{};".format(pressure_set_kPa)
+        cmd_string = "PUMP:{};\n".format(pressure_set_kPa)
         response = self._send_command_str(cmd_string)
         return response
+
+    def parse_command(self, command_string: str) -> str:
+        command_list = command_string.split(" ")
+        command_type = command_list[0].upper()
+        command_content = command_list[1]
+        if command_type == "PUMP":
+            return self.set_pressure_pump(float(command_content))
+        elif command_type == "RESSWITCH":
+            return self.res_switch(command_content == "1")
+        elif command_type == "DUMPSWITCH":
+            return self.dump_switch(command_content == "1")
+        else:
+            raise ValueError("Command Type was not one of {PUMP/RESSWITCH/DUMPSWITCH}")
+        
 
 
 
@@ -66,11 +82,8 @@ if __name__ == '__main__':
     print("Pyserial Version:", serial.__version__)
     print('Running. Press CTRL-C to exit.')
     with PressureController() as pres:
-        print("Set pump pressure")
-        print(pres.set_pressure_pump(-15.0))
-        print("Open Res Switch")
-        print(pres.res_switch(1))
-        print("Close Res Switch")
-        print(pres.res_switch(0))
-        print("Open Dump Switch")
-        print(pres.dump_switch(1))
+        while True:
+            cmd = input("Enter Command: {PUMP (float), RESSWITCH (0/1), DUMPSWITCH (0/1)")
+            print(pres.parse_command(cmd))
+
+
