602c4b8e7922f12adfa60f2690a1ca5f74df02ff
Will Brown <brown.wi@husky.neu.edu>
2022-05-24 13:45:17 -0400

    Initial preview table logic working

--- cd_alpha/protocols/protocol_tools.py
+++ cd_alpha/protocols/protocol_tools.py
@@ -22,13 +22,15 @@ class ProcessProtocol:
 
     def list_steps(self):
         step_number = 0
+        incubate = 0
+        list_of_table_entries = []
         print("Step # \t\tMaterial \t\tFlowrate(mL/h) \t\tVolume(mL) \t\tStep Duration")
         for key in self.protocol.keys():
-            step_number += 1
             for k in self.protocol[key]:
                 if k == "action":
                     for steps in self.protocol[key][k]:
                         if steps == "PUMP":
+                            step_number += 1
                             for s in self.protocol[key][k][steps]:
                                 if s == "vol_ml":
                                     volume = self.protocol[key][k][steps][s]
@@ -37,9 +39,14 @@ class ProcessProtocol:
                                 elif s == "eq_time":
                                     duration = self.protocol[key][k][steps][s]
                             material = self.protocol[key]["header"].split(" ")[0]
-                            print("{} \t\t\t{} \t\t\t{} \t\t\t\t\t{} \t\t\t\t\t{}".format(step_number, material, flowrate, volume, duration))
+                            list_of_table_entries.append([step_number, material, flowrate, volume, duration])
                         elif steps == "INCUBATE":
                             incubate = self.protocol[key][k][steps]["time"]
+                            list_of_table_entries[-1][-1]+= incubate
+        
+        for line in list_of_table_entries:
+            print(line)
+                            
 
     def calculate_step_time(self, step: OrderedDict):
         step["time"]          
