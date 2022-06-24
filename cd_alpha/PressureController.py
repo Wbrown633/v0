2fbfe6a4e3318a8bc1aa1f62b8d666b0b174977e
Will Brown <will.brown@chip-diagnostics.com>
2022-06-24 15:45:16 -0400

    Summary page (#8)
    * Working on pressure controller first draft
    
    * Add wait to listen for arduino response
    
    * Try reading all lines from arduino when we get a response
    
    * add line limit
    
    * update baud rate
    
    * Remove use of deprecated function
    
    * Use readline()
    
    * Debugging readlines
    
    * use writelines method
    
    * reset output buffer
    
    * Try another character limit
    
    * remove more deprecated functions
    
    * fix method error in output flush
    
    * increase timeout for use over ssh
    
    * use write not writelines()
    
    * Back to readline
    
    * Dont flush output
    
    * try new read serial method
    
    * add more wait
    
    * try readlines
    
    * Lower char limit in readlines
    
    * try to reset input buffer
    
    * show how much is in the input buffer
    
    * bigger readlines buffer
    
    * New pressure controller class
    
    * Using context manager
    
    * Fix type hinting
    
    * fix call to self.arduino
    
    * encoding issue
    
    * Increase wait times
    
    * clear input buffer on initialize
    
    * update initialization information
    
    * add back in wait for command response to come in
    
    * fix typo on wait
    
    * fix issue with wait
    
    * add more steps in test
    
    * Fix sending self to wrong method
    
    * Properly clear input buffer
    
    * print entire response
    
    * debugging serial issues
    
    * trying to fix flushing buffer
    
    * try flush method
    
    * add some more sleeps on flush
    
    * test flush
    
    * more debug for flush
    
    * get rid of readlines method
    
    * try without timeout
    
    * try readlines
    
    * will this make an inf loop?
    
    * add sleep
    
    * debug reading off serial port
    
    * Debug multi line serial read
    
    * more issues with input buffer
    
    * create interactive shell for Pressure controller testing
    
    * Switch back to pi serial port
    
    * Update git ignore
    
    * Work on pressure controller and stub for gui testing
    
    * Working on gui testing of pressure drive with protocols
    
    * pressure controller works with gui and a test protocol
    
    * Fix naming issue in setup files
    
    * Working on table preview for protocol
    
    * working on table printing for 16v3
    
    * Initial preview table logic working
    
    * Begin work on summary screen
    
    * more work on summary screen
    
    * Basic summary screen complete
    
    * Fix formatting of duration in
    
    * Prototype of summary screen working
    
    * change protocols to support summary page

---
+++ cd_alpha/PressureController.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# lsusb to check device name
+#dmesg | grep "tty" to find port name
+
+from typing import List
+import serial,time
+import logging
+from kivy.clock import Clock
+
+class PressureController:
+    def __init__(self) -> None:
+        self.max_pressure_set_point_kPa = 120
+        self.flow_rate_ml_per_hr = None
+        self.volume_ml = None
+            
+    def __enter__(self) -> None: 
+        logging.info("Opening Serial connection")
+        self.arduino = serial.Serial("/dev/ttyACM0", 115200, timeout=0) #for pi its "/dev/ttyACM0" for win it's usually "COM3"
+        time.sleep(1)
+        print("{} connected!".format(self.arduino.port))
+        init_msg = self._read_input()
+        print("Got init msg: {}".format(init_msg))
+        return self
+        
+    def __exit__(self, exc_type, exc_value, exc_traceback): 
+        print("Closing serial port.")
+        self.arduino.flush()
+        self.arduino.close()
+
+    def _send_command_str(self, cmd: str) -> List[str]:
+        assert self.arduino.isOpen(), "Serial port not open."
+        self.arduino.write(cmd.encode())
+        list_of_input = self._read_input()
+        return list_of_input
+
+    def _read_input(self) -> str:
+        confirmation_msg = []
+        time.sleep(1.5)
+        while self.arduino.in_waiting > 0:
+            confirmation_msg.append(self.arduino.readline().decode())
+        return confirmation_msg
+
+    def _switch_status(self, switch_name:str, status:bool) -> str:
+        '''Change the status of the Reservoir switch. Return the response string.'''
+        switch_status = 0
+        if status:
+            switch_status = 1
+        cmd_string = "{}:{};\n".format(switch_name, switch_status)
+        response = self._send_command_str(cmd_string)
+        return response
+
+    def _calculate_time_secs(self) -> int:
+        '''Given a the flowrate and volume to be pumped calculate the time required.'''
+        # TODO make sure this level of accuracy is enough, int rounding could lose information
+        return int(self.volume_ml / self.flow_rate_ml_per_hr * 3600)
+
+    def _pressure_from_flowrate(self) -> float:
+        '''Using provided calibration curves calculate the pressure needed to achieve the desired flowrate.'''
+
+        #TODO these curves may need to depend on the liquid that is being used, make a dictionary of fluid type -> 2 point calibration equation
+
+        return self.flow_rate_ml_per_hr * 0.12 - 100.0  # Totally made up place holder curve
+        
+    def res_switch(self, status:bool) -> str:
+        return self._switch_status("RESSWITCH", status)
+    
+    def dump_switch(self, status:bool) -> str:
+        '''Change the status of the pressure dump switch. Return the response string.'''
+        return self._switch_status("DUMPSWITCH", status)
+        
+    def set_pressure_pump(self, pressure_set_kPa: float) -> str:
+        '''Change the pressure set point for the pump.'''
+        assert pressure_set_kPa < self.max_pressure_set_point_kPa, "Invalid pressure"
+        cmd_string = "PUMP:{};\n".format(pressure_set_kPa)
+        response = self._send_command_str(cmd_string)
+        return response
+
+    def get_pressure_reading(self) -> float:
+        # needs to be implemented in the Arduino
+        pass 
+
+    def parse_command(self, command_string: str) -> str:
+        command_list = command_string.split(" ")
+        command_type = command_list[0].upper()
+        command_content = command_list[1]
+        if command_type == "PUMP":
+            return self.set_pressure_pump(float(command_content))
+        elif command_type == "RESSWITCH":
+            return self.res_switch(command_content == "1")
+        elif command_type == "DUMPSWITCH":
+            return self.dump_switch(command_content == "1")
+        else:
+            raise ValueError("Command Type was not one of {PUMP/RESSWITCH/DUMPSWITCH}")
+
+    def set_rate(self, rate, unit="MH", addr=''):
+        self.flow_rate_ml_per_hr = rate
+
+    def set_volume(self, volume, unit="ML", addr=''):
+        self.volume_ml = volume
+
+    def stop_all_pumps(self, list_of_pumps):
+        self.set_pressure_pump(100.0)
+
+    def release_pressure(self, dt):
+        print("Pressure released after : {} seconds".format(dt))
+        self.res_switch(False) # at the end of the step close the res switch
+        self.dump_switch(True) # open the release valve to stop the pressure on the chip
+
+    def run(self, addr="0") -> None:
+
+        ''' Step logic:
+            - set the pressure of the pump
+            - when the pressure in the reservoir is achieved open the res switch
+            - keep the res switch open for the duration period
+            - close res switch, open dump switch
+        '''
+
+        
+        logging.info(self.set_pressure_pump(self._pressure_from_flowrate()))
+
+        # these waits should be re-factored to use either kivy clock or async await, this is just a rough draft
+
+        # Using sleeps for proof of concept, will block GUI and is really ugly
+        # Open the res_switch
+        self.res_switch(True)
+        
+        # Wait for the provided step duration
+        Clock.schedule_once(self.release_pressure, self._calculate_time_secs())
+        print("Seconds to wait for step time: {}".format(self._calculate_time_secs()))
+
+        
+
+
+
+if __name__ == '__main__':
+
+    print("Pyserial Version:", serial.__version__)
+    print('Running. Press CTRL-C to exit.')
+    with PressureController() as pres:
+        while True:
+            cmd = input("Enter Command: {PUMP (float), RESSWITCH (0/1), DUMPSWITCH (0/1)")
+            print(pres.parse_command(cmd))
+
+
