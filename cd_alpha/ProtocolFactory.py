0a6d5367f12855d82e85b33f51f643e714310172
Will Brown <will.brown@chip-diagnostics.com>
2022-08-05 16:57:10 -0400

    Begin defining default steps for protocols

--- cd_alpha/ProtocolFactory.py
+++ cd_alpha/ProtocolFactory.py
@@ -8,10 +8,6 @@ import json
 from typing import Dict, List
 
 
-
-
-TEST_FILE_LOCATION = "test_output.json"
-
 @dataclass
 class StepType(Enum):
     PUMP = 1
@@ -52,7 +48,7 @@ class Release(Pump):
 class Incubate(ActionType):
     time: int
 
-
+@dataclass
 class Reset(ActionType):
     def make_dict(self) -> Dict:
         return {"RESET": {}}
@@ -72,9 +68,7 @@ class ScreenType(Enum):
 @dataclass
 class Step:
     material: str
-    step_number: int
     description_text: str
-    screentype: ScreenType
     steptype: StepType
     target: Target
     volume: float
@@ -142,17 +136,47 @@ class ProtocolFactory:
     
     def __init__(self, list_of_steps: List[StepBuilder]):
         self.list_of_steps = list_of_steps
-    
-    def create_step(self):
-        # PSEUDO CODE
 
-        # Create a JSON protocol given the following information for every step
-        # TYPE: {PUMP, INCUBATE, PUMP/RELEASE}
-        # STEP_INFO: [TARGET, VOL_ML, RATE_MH, EQ_TIME_SECS]
+    def create_protocol(self, output_file: str):
+        '''Creates and exports a json protocol at the givent file location.'''
+
+        ## Add default steps before and after pump steps ### 
+
+        self._add_default_steps()
+        self.json_dump(output_file)
+
+    def _add_default_steps(self):
+        SETUP_STEPS = self._define_setup_steps()
+        TEARDOWN_STEPS = self._define_teardown_steps()
+        
+        self.list_of_steps.insert(0,SETUP_STEPS)
+        self.list_of_steps.append(TEARDOWN_STEPS)
+
+    def _define_setup_steps(self) -> List[Step]:
+        list_of_steps = []
+
+        home_step = StepBuilder("home").add_type(ScreenType.UserActionScreen).add_header("Chip Diagnostics")\
+            .add_description("Ready for a new test with protocol 20v0. \
+                Press 'Start' to begin.").add_next_text("Start").getStep()
+
+        reset_start_step = StepBuilder("reset_start").add_type(ScreenType.MachineActionScreen)\
+            .add_header("Initialization").add_description("Initializing device. Resetting syringe positions and checking connections.")\
+                .add_actions([Reset()]).remove_progress_bar(True).add_completion_msg("Machine has been homed").getStep()
+
+        insert_syringes_step = StepBuilder("insert_syringes").add_type(ScreenType.UserActionScreen).add_header("Insert syringes")\
+            .add_description("Insert the waste and lysate syringe. Press 'Start initialization' to initialize when ready.").add_next_text("Start initialization").getStep()
+
+
+        grab_syringes_step = StepBuilder("grab_syringes").add_type(ScreenType.MachineActionScreen).add_header("Grabbing syringes")\
+            .add_description("The device is now grabbing hold of the syringes to secure a precise operation.").add_actions([Grab(5,0.3)]).remove_progress_bar(True).add_completion_msg("Syringes ready").getStep()
+    
+    
+    
+    def _define_teardown_steps(self) -> List[Step]:
         pass
 
-    def json_dump(self):
-        with open(TEST_FILE_LOCATION, 'w') as f:
+    def json_dump(self, file_location: str):
+        with open(file_location, 'w') as f:
             steps_dictionary = {}
             for s in self.list_of_steps:
                 steps_dictionary.update(s.getStep())  
@@ -169,6 +193,7 @@ if __name__ == "__main__":
     list_of_steps = [s1, s2, s3]
 
     p = ProtocolFactory(list_of_steps)
+    TEST_FILE_LOCATION = "test_output.json"
 
     print("Making json dump ")
-    p.json_dump()
+    p.json_dump(TEST_FILE_LOCATION)
--- cd_alpha/tests/test_protocol_factory.py
+++ cd_alpha/tests/test_protocol_factory.py
@@ -1,5 +1,10 @@
+
 import unittest
+from collections import OrderedDict
+
+from pkg_resources import resource_filename
 from cd_alpha.ProtocolFactory import ProtocolFactory, StepBuilder
+import json
 
 
 class ProtocolFactoryTestCase(unittest.TestCase):
@@ -9,8 +14,26 @@ class ProtocolFactoryTestCase(unittest.TestCase):
         pass
         
 
-    def test_skip_and_reschedule(self):
-        pass
+    def test_protocol_factory_20v0(self):
+        test_file = resource_filename("cd_alpha", "protocols/v0-protocol-20v0.json")
+
+        with open(test_file, 'r') as f:
+            protocol = json.loads(f.read(), object_pairs_hook=OrderedDict)
+
+
+        # PSEUDO CODE 
+        # make the list of steps needed to replicate protocol 20v0 using the Protocol factory method
+
+        list_of_steps = [StepBuilder("test").getStep()]
+
+        ProtocolFactory(list_of_steps).create_protocol("test.json")
+
+        with open("test.json", 'r') as f:
+            test_protocol = json.loads(f.read(), object_pairs_hook=OrderedDict)
+
+        self.assertEqual(protocol, test_protocol)
+
+
 
 
 
