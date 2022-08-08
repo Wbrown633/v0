61d671bdf844478d7ab73b02d29cc2cdf3874f74
Will Brown <will.brown@chip-diagnostics.com>
2022-08-08 12:11:32 -0400

    trying to make protocol factory test pass

--- cd_alpha/ProtocolFactory.py
+++ cd_alpha/ProtocolFactory.py
@@ -141,39 +141,48 @@ class ProtocolFactory:
         '''Creates and exports a json protocol at the givent file location.'''
 
         ## Add default steps before and after pump steps ### 
-
         self._add_default_steps()
         self.json_dump(output_file)
 
     def _add_default_steps(self):
-        SETUP_STEPS = self._define_setup_steps()
-        TEARDOWN_STEPS = self._define_teardown_steps()
-        
-        self.list_of_steps.insert(0,SETUP_STEPS)
-        self.list_of_steps.append(TEARDOWN_STEPS)
+        for step in self._define_setup_steps():
+            self.list_of_steps.insert(0,step)
+        for s in self._define_teardown_steps():
+            self.list_of_steps.append(s)
 
     def _define_setup_steps(self) -> List[Step]:
-        list_of_steps = []
-
+   
         home_step = StepBuilder("home").add_type(ScreenType.UserActionScreen).add_header("Chip Diagnostics")\
             .add_description("Ready for a new test with protocol 20v0. \
-                Press 'Start' to begin.").add_next_text("Start").getStep()
+                Press 'Start' to begin.").add_next_text("Start")
 
         reset_start_step = StepBuilder("reset_start").add_type(ScreenType.MachineActionScreen)\
             .add_header("Initialization").add_description("Initializing device. Resetting syringe positions and checking connections.")\
-                .add_actions([Reset()]).remove_progress_bar(True).add_completion_msg("Machine has been homed").getStep()
+                .add_actions([Reset()]).remove_progress_bar(True).add_completion_msg("Machine has been homed")
 
         insert_syringes_step = StepBuilder("insert_syringes").add_type(ScreenType.UserActionScreen).add_header("Insert syringes")\
-            .add_description("Insert the waste and lysate syringe. Press 'Start initialization' to initialize when ready.").add_next_text("Start initialization").getStep()
+            .add_description("Insert the waste and lysate syringe. Press 'Start initialization' to initialize when ready.").add_next_text("Start initialization")
 
 
         grab_syringes_step = StepBuilder("grab_syringes").add_type(ScreenType.MachineActionScreen).add_header("Grabbing syringes")\
-            .add_description("The device is now grabbing hold of the syringes to secure a precise operation.").add_actions([Grab(5,0.3)]).remove_progress_bar(True).add_completion_msg("Syringes ready").getStep()
+            .add_description("The device is now grabbing hold of the syringes to secure a precise operation.").add_actions([Grab(5,0.3)]).remove_progress_bar(True).add_completion_msg("Syringes ready")
+
+
+        insert_chip_step = StepBuilder("insert_chip").add_type(ScreenType.UserActionScreen).add_header("Insert Kit").add_description("Insert the chip and inlet reservoir. Press 'Next' to proceed.")
     
+        step_list = [home_step, reset_start_step, insert_syringes_step, grab_syringes_step, insert_chip_step]
+        step_list.reverse()
+        return step_list
     
     
     def _define_teardown_steps(self) -> List[Step]:
-        pass
+        
+        remove_kit_step = StepBuilder("remove_kit").add_type(ScreenType.UserActionScreen).add_header("Remove kit").add_description("Remove used kit from machine.")
+
+        reset_end_step = StepBuilder("reset_end").add_type(ScreenType.MachineActionScreen).add_header("Homing device").add_description("Resetting syringe pump positions.")\
+            .add_actions([Reset()]).remove_progress_bar(True).add_completion_msg("App will restart so a new test may begin.")
+
+        return [remove_kit_step, reset_end_step]
 
     def json_dump(self, file_location: str):
         with open(file_location, 'w') as f:
@@ -190,10 +199,7 @@ if __name__ == "__main__":
     s2 = StepBuilder("reset_start").add_type(ScreenType.MachineActionScreen).add_header("Initialization").add_description("Init device").add_actions([Reset()]).remove_progress_bar(True).add_completion_msg("Machine has been homed")
     s3 = StepBuilder("flush_2").add_type(ScreenType.MachineActionScreen).add_header("PBS rinse").add_description("Risning the chip.").add_actions([Pump("waste", 1.05, 50, 120)])
 
-    list_of_steps = [s1, s2, s3]
+    list_of_stepbuilders = [s1, s2, s3]
 
-    p = ProtocolFactory(list_of_steps)
-    TEST_FILE_LOCATION = "test_output.json"
-
-    print("Making json dump ")
-    p.json_dump(TEST_FILE_LOCATION)
+    ProtocolFactory(list_of_stepbuilders).create_protocol("protocol_factory.json")
+    
--- cd_alpha/tests/test_protocol_factory.py
+++ cd_alpha/tests/test_protocol_factory.py
@@ -1,9 +1,10 @@
 
+from typing import List
 import unittest
 from collections import OrderedDict
 
 from pkg_resources import resource_filename
-from cd_alpha.ProtocolFactory import ProtocolFactory, StepBuilder
+from cd_alpha.ProtocolFactory import ProtocolFactory, ScreenType, Step, StepBuilder
 import json
 
 
@@ -24,7 +25,7 @@ class ProtocolFactoryTestCase(unittest.TestCase):
         # PSEUDO CODE 
         # make the list of steps needed to replicate protocol 20v0 using the Protocol factory method
 
-        list_of_steps = [StepBuilder("test").getStep()]
+        list_of_steps = self.protocol_factory_make_20v0()
 
         ProtocolFactory(list_of_steps).create_protocol("test.json")
 
@@ -32,6 +33,50 @@ class ProtocolFactoryTestCase(unittest.TestCase):
             test_protocol = json.loads(f.read(), object_pairs_hook=OrderedDict)
 
         self.assertEqual(protocol, test_protocol)
+
+    def protocol_factory_make_20v0() -> List[StepBuilder]:
+        f127 = StepBuilder("f127").add_type(ScreenType.UserActionScreen).add_header("Add F-127")\
+            .add_description("Add 1.4 mL 1% F-127 in 1xPBS to reservoir. Press 'Next' to start.")
+        
+        flush_1 = StepBuilder("flush_1")
+
+        incubate_1 = StepBuilder("incubate_1")
+
+        pbs_1 = StepBuilder("pbs_1")
+
+        flush_2 = StepBuilder("flush_2")
+
+        add_sample = StepBuilder("add_sample")
+
+        flush_3 = StepBuilder("flush_3")
+
+        pbs_2 = StepBuilder("pbs_2")
+
+        wash_1 = StepBuilder("wash_1")
+
+        pbs_3 = StepBuilder("pbs_3")
+
+        flush_5 = StepBuilder("flush_5")
+
+        flush_6 = StepBuilder("flush_6")
+
+        pbs_4 = StepBuilder("pbs_4")
+
+        flush_7 = StepBuilder("flush_7")
+
+        qiazol = StepBuilder("qiazol")
+
+        extract_1 = StepBuilder("extract_1")
+
+        extract_2 = StepBuilder("extract_2")
+
+        PBSchase = StepBuilder("PBSchase")
+
+        chase_1 = StepBuilder("chase_1")
+
+
+
+        
 
 
 
