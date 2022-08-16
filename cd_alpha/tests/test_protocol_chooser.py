46d840ffb79d0d6e7dea7410aa45d91e0b5b94de
Will Brown <will.brown@chip-diagnostics.com>
2022-08-16 16:22:03 -0400

    begin working on pytest

--- cd_alpha/tests/test_protocol_chooser.py
+++ cd_alpha/tests/test_protocol_chooser.py
@@ -1,8 +1,8 @@
 import pytest
 from cd_alpha.ChipFlowApp import ProcessWindow
 
-class TestProtocolChooserCase():
 
+class TestProtocolChooserCase:
     def setUp(self):
         # import class and prepare everything here.
         self.test_window = ProcessWindow(protocol_file_name="v0-protocol-16v1.json")
@@ -13,16 +13,16 @@ class TestProtocolChooserCase():
         # create a test window and check that all of the required screens are added
         self.test_window.load_protocol(self.test_protocol_location)
 
-        # For 16v1 there should be 35 screens. {'protocol_chooser': 1, 'home': 1, 'reset_start': 1, 'reset_start_done': 1, 
-        # 'insert_syringes': 1, 'grab_syringes': 1, 'grab_syringes_done': 1, 'insert_chip': 1, 'f127': 1, 'flush_1': 1, 
-        # 'incubate_1': 1, 'incubate_1_done': 1, 'pbs_1': 1, 'flush_2': 1, 'flush_2_done': 1, 'add_sample': 1, 'flush_3': 1, 
-        # 'flush_3_done': 1, 'pbs_2': 1, 'wash_1': 1, 'wash_1_done': 1, 'pbs_3': 1, 'flush_5': 1, 'flush_5_done': 1, 'pbs_4': 1, 
-        # 'flush_6': 1, 'flush_6_done': 1, 'qiazol': 1, 'extract_1': 1, 'extract_1_done': 1, 'PBSchase': 1, 'chase_1': 1, 
+        # For 16v1 there should be 35 screens. {'protocol_chooser': 1, 'home': 1, 'reset_start': 1, 'reset_start_done': 1,
+        # 'insert_syringes': 1, 'grab_syringes': 1, 'grab_syringes_done': 1, 'insert_chip': 1, 'f127': 1, 'flush_1': 1,
+        # 'incubate_1': 1, 'incubate_1_done': 1, 'pbs_1': 1, 'flush_2': 1, 'flush_2_done': 1, 'add_sample': 1, 'flush_3': 1,
+        # 'flush_3_done': 1, 'pbs_2': 1, 'wash_1': 1, 'wash_1_done': 1, 'pbs_3': 1, 'flush_5': 1, 'flush_5_done': 1, 'pbs_4': 1,
+        # 'flush_6': 1, 'flush_6_done': 1, 'qiazol': 1, 'extract_1': 1, 'extract_1_done': 1, 'PBSchase': 1, 'chase_1': 1,
         # 'remove_kit': 1, 'reset_end': 1, 'reset_end_done': 1}
         assert len(self.test_window.process_sm.screens) == 35
 
-        # Check that there are no duplicate steps 
-        self.assertFalse(self._find_duplicates(self.test_window.process_sm.screen_names))
+        # Check that there are no duplicate steps
+        assert not self._find_duplicates(self.test_window.process_sm.screen_names)
 
     # Test that loading protocols multiple times in a row doesn't cause duplicate steps
     def test_protocol_load_multiple(self):
@@ -45,12 +45,11 @@ class TestProtocolChooserCase():
         with pytest.raises(TypeError):
             self.test_window.load_protocol("invalid_protocol.json")
 
-
     def test_load_from_archive(self):
         pass
 
-
     def _find_duplicates(self, list_of_values):
-        return any(self.test_window.process_sm.screen_names.count(value) > 1 for value in list_of_values)
-
-
+        return any(
+            self.test_window.process_sm.screen_names.count(value) > 1
+            for value in list_of_values
+        )
