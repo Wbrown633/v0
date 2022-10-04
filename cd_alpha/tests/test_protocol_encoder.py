1ddf2dcac57ca0bb3bd061cd9827c6e5ecaa856c
Will Brown <williamlandisbrown@gmail.com>
2022-10-04 14:14:01 -0400

    test for encoder with 21v3 minor fixes

--- cd_alpha/tests/test_protocol_encoder.py
+++ cd_alpha/tests/test_protocol_encoder.py
@@ -50,32 +50,32 @@ class TestProtocolEncoder:
         s = Step("flush_1", [a])
 
         a1 = Incubate("F-127", 3600)
-        s1 = Step("Incubate", [a1])
+        s1 = Step("incubate_1", [a1])
 
         a2 = Pump(material="F-127", target="waste", vol_ml= 0.5, rate_mh= 15, eq_time=0)
         s2 = Step("flush_2", [a2])
 
         a3 = Pump(material="PBS", target="waste", vol_ml= 1.0, rate_mh= 15, eq_time=0)
-        s3 = Step("pbs_1", [a3])
+        s3 = Step("flush_3", [a3])
 
         a4 = Pump(material="Sample", target="waste", vol_ml= 0.5, rate_mh= 1.0, eq_time=0)
-        s4 = Step("add_sample", [a4])
+        s4 = Step("flush_4", [a4])
 
         a5 = Pump(material="PBS", target="waste", vol_ml= 0.7, rate_mh= 15.0, eq_time=0)
-        s5 = Step("pbs_2", [a5])
+        s5 = Step("wash_1", [a5])
 
         a6 = Pump(material="PBS", target="waste", vol_ml= 0.7, rate_mh= 15.0, eq_time=0)
-        s6 = Step("pbs_3", [a6])
+        s6 = Step("flush_5", [a6])
 
         a7 = Pump(material="PBS", target="waste", vol_ml= 0.7, rate_mh= 15.0, eq_time=0)
-        s7 = Step("pbs_4", [a7])
+        s7 = Step("flush_6", [a7])
 
         a8 = Pump(material="Qiazol", target="lysate", vol_ml= 0.2, rate_mh= 15.0, eq_time=150)
         a9 = Release(target="waste", vol_ml=1.5, rate_mh=-50, eq_time=0)
-        s8 = Step("qiazol", [a8, a9])
+        s8 = Step("extract_1", [a8, a9])
 
         a10 = Pump(material="Qiazol", target="lysate", vol_ml= 1.0, rate_mh= 15.0, eq_time=0)
-        s9 = Step("extract", [a10])
+        s9 = Step("chase_1", [a10])
 
         p.add_steps([s, s1, s2, s3, s4, s5, s6, s7, s8, s9])
 
@@ -94,7 +94,7 @@ class TestProtocolEncoder:
 
         output_file = "encoder_21v3.json"
 
-        encoder.make_json_protocol_file("200v0", output_file)
+        encoder.make_json_protocol_file("21v3", output_file)
 
         with open(pathlib.Path(output_file), "r") as f: 
             json_string = json.load(f)
